ae33d707bbee83771828d58c54178e0ef07ee82e
drkbldwn208 <bderek731@gmail.com>
2026-05-06 13:49:10 -0700

    Changed dataflow structure

--- src/dy_cavity_relocker_2.cpp
+++ src/dy_cavity_relocker_2.cpp
@@ -14,28 +14,34 @@ static void read_adc(hls::stream<axis_t> &adc_in,
 }
 
 static void decimate_by_4(hls::stream<short> &in, hls::stream<short> &out) {
-  int32_t acc = 0;
-  for (int i = 0; i < 128; i++) {
-    #pragma HLS PIPELINE II=1
-    acc += in.read();
+  #pragma HLS PIPELINE II=1
+  static int32_t acc = 0;
+  static int count = 0;
 
-    // Every 4th sample, write the output and reset the accumulator
-    if ((i % 4) == 3) {
-      out.write((short)(acc >> 2));
-      acc = 0;
-    }
+  acc += in.read();
+  count++;
+
+  if (count==4) {
+    out.write((short)(acc >> 2)); // Average of 4 samples
+    acc = 0;
+    count = 0;
   }
 }
 
 static void decimate_by_32(hls::stream<short> &in,
-                            hls::stream<short> &out)
-{
-    long acc = 0;
-    for (int i = 0; i < 32; i++) {
-        #pragma HLS PIPELINE II=1
-        acc += in.read();
-    }
-    out.write((short)(acc >> 5));
+                            hls::stream<short> &out) {
+  #pragma HLS PIPELINE II=1
+  static long acc = 0;
+  static short count = 0;
+
+  acc += in.read();
+  count++;
+
+  if (count == 32) {
+    out.write((short)(acc >> 5)); // Average of 32 samples
+    acc = 0;
+    count = 0;
+  }
 }
 
 static void hinf_path(hls::stream<short> &ch1_in, 
@@ -55,25 +61,17 @@ static void servo_path(hls::stream<short> &ch2_in,
                        bool gpio_in,
                        int servo_offset,
                        int servo_arm) {
-
+  #pragma HLS PIPELINE II=1
   static State current_state = IDLE;
   static short held_voltage = 0;
   static short error_signal = 0;
   
-  for (int i = 0; i < 128; i++) {
-    #pragma HLS PIPELINE II=1
-    
+  bool state_toggle = false;
+  fsm_receiver(gpio_in, state_toggle);
+  short val_adc2 = ch2_in.read();
+  short out=0;
 
-    bool state_toggle = false;
-
-    fsm_receiver(gpio_in, state_toggle);
-
-    short val_adc2 = ch2_in.read();
-
-    short out=0;
-
-    switch (current_state)
-    {
+  switch (current_state) {
     case IDLE:
       out=servo_offset; // Output the servo offset to DAC channel 2 in IDLE state
       if (servo_arm && state_toggle) {
@@ -81,7 +79,6 @@ static void servo_path(hls::stream<short> &ch2_in,
         held_voltage = val_adc2; // Capture the current ADC value to hold in SERVO state
       }
       break;
-
 
     case SERVO:
       error_signal = val_adc2 - held_voltage;
@@ -92,43 +89,43 @@ static void servo_path(hls::stream<short> &ch2_in,
         current_state = IDLE;
       }
       break;
-    }
-
-    dac2_out.write(out);
   }
+  dac2_out.write(out);
 }
 
 static void hold_128(hls::stream<short> &in,
-                   hls::stream<short> &out) 
-{
-  short val = in.read();
-  for (int i = 0; i < 128; i++) {
-    #pragma HLS PIPELINE II=1
-    out.write(val);
+                   hls::stream<short> &out) {
+
+  #pragma HLS PIPELINE II=1
+  static short current_val = 0;
+  short new_val;
+
+  if (in.read_nb(new_val)) {
+    current_val = new_val;
   }
+  out.write(current_val);
 }
+
 
 static void pack_dac(hls::stream<short> &dac1_in,
                      hls::stream<short> &dac2_in,
-                     hls::stream<axis_t> &dac_out)
-{
-  for (int i = 0; i < 128; i++) {
-    #pragma HLS PIPELINE II=1
-    axis_t val_dac;
-    // short dac_1_out = dac1_in.read();
-    // short dac_2_out = dac2_in.read();
-    dac1_in.read(); // D/iscard the H-infinity path output for now
-    dac2_in.read(); // Discard the servo path output for now
-    short dac_1_out = 200;
-    short dac_2_out = -200;
-    val_dac.data = (((uint32_t)dac_2_out & 0xFFFF) << 16) | ((uint32_t)dac_1_out & 0xFFFF);
-    val_dac.keep = 0xF;
-    val_dac.strb = 0xF;
-    val_dac.last = 0;
-
-    dac_out.write(val_dac);
-  }
+                     hls::stream<axis_t> &dac_out) {
+  #pragma HLS PIPELINE II=1
+  axis_t val_dac;
+  // short dac_1_out = dac1_in.read();
+  // short dac_2_out = dac2_in.read();
+  dac1_in.read(); // D/iscard the H-infinity path output for now
+  dac2_in.read(); // Discard the servo path output for now
+  short dac_1_out = 200;
+  short dac_2_out = -200;
+  val_dac.data = (((uint32_t)dac_2_out & 0xFFFF) << 16) | ((uint32_t)dac_1_out & 0xFFFF);
+  val_dac.keep = 0xF;
+  val_dac.strb = 0xF;
+  val_dac.last = 0;
+  
+  dac_out.write(val_dac);
 }
+
 
 
 void dy_cavity_relocker_2(hls::stream<axis_t> &adc_in,
@@ -143,15 +140,10 @@ void dy_cavity_relocker_2(hls::stream<axis_t> &adc_in,
   #pragma HLS INTERFACE s_axilite port = servo_offset
   #pragma HLS INTERFACE s_axilite port = servo_arm
   #pragma HLS INTERFACE ap_none port = gpio_in
-  #pragma HLS INTERFACE s_axilite port = return
+  #pragma HLS INTERFACE ap_ctrl_none port = return
 
   hls::stream<short> ch1, ch1_decim, ch1_decim_32, ch2, dac1_raw, dac1, dac2;
-  #pragma HLS STREAM variable = ch1 depth = 8
-  #pragma HLS STREAM variable = ch1_decim depth = 4
-  #pragma HLS STREAM variable = ch1_decim_32 depth = 4
   #pragma HLS STREAM variable = ch2 depth = 256
-  #pragma HLS STREAM variable = dac1_raw depth = 4
-  #pragma HLS STREAM variable = dac1 depth = 8
   #pragma HLS STREAM variable = dac2 depth = 256
   
   read_adc(adc_in, ch1, ch2);
